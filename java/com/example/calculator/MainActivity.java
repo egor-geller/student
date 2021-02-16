01eb60b7975fa5f796865a82336df6d53d03492b
egor-geller <56612094+egor-geller@users.noreply.github.com>
2021-02-17 01:09:03 +0300

    Update MainActivity.java

--- java/com/example/calculator/MainActivity.java
+++ java/com/example/calculator/MainActivity.java
@@ -1,258 +1,143 @@
-package com.example.calculator;
+package com.example.opitulab1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
 import java.text.DecimalFormat;
-import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView textView, textView2;
-    private boolean hasPoint = false;
+public class MainActivity extends Activity {
+    private TextView resultTextView;
+    private TextView signTextView;
+    private TextView enterEditText;
+    private String lastSign = "=";
+    private Double lastValue = null;
 
-    DecimalFormat format = new DecimalFormat("0.#");
+    DecimalFormat format = new DecimalFormat("0.##########");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initViews();
+        resultTextView = findViewById(R.id.resultTextView);
+        enterEditText = findViewById(R.id.enterEditText);
+        signTextView = findViewById(R.id.signTextView);
     }
 
-    private void initViews() {
-        findViewById(R.id.btn0);
-        findViewById(R.id.btn1);
-        findViewById(R.id.btn2);
-        findViewById(R.id.btn3);
-        findViewById(R.id.btn4);
-        findViewById(R.id.btn5);
-        findViewById(R.id.btn6);
-        findViewById(R.id.btn7);
-        findViewById(R.id.btn8);
-        findViewById(R.id.btn9);
-        findViewById(R.id.btnAC);
-        findViewById(R.id.btnReverse);
-        findViewById(R.id.btnDivide);
-        findViewById(R.id.btnMult);
-        findViewById(R.id.btnMinus);
-        findViewById(R.id.btnPlus);
-        findViewById(R.id.btnEqual);
-        findViewById(R.id.btnComma);
-        findViewById(R.id.btnPercentage);
-
-        textView = findViewById(R.id.textView);
-        textView2 = findViewById(R.id.textView2);
-        textView.setText("0");
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("OPERATION", lastSign);
+        if (lastValue != null) {
+            outState.putDouble("lastValue", lastValue);
+        }
+        if (enterEditText != null) {
+            outState.putString("currentValue", enterEditText.getText().toString());
+        }
+        super.onSaveInstanceState(outState);
     }
 
-    double first = 0;
-    double second = 0;
-    double result = 0;
-    String oper = "";
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        lastSign = savedInstanceState.getString("OPERATION");
+        lastValue = savedInstanceState.getDouble("lastValue");
+        String currentValue = savedInstanceState.getString("currentValue");
+        resultTextView.setText(format.format(lastValue));
+        enterEditText.setText(currentValue);
+        signTextView.setText(lastSign);
+    }
 
-    public void onClickBtn1(View v) {
-        if (textView.getText() == "0") {
-            textView.setText("1");
+    public void onNumberClick(View view) {
+        if (lastSign.equals("=") && lastValue != null) {
+            lastValue = null;
+        }
+        enterEditText.append(((Button) view).getText());
+    }
+
+    public void onDelete(View view) {
+        if (enterEditText.getText().toString().length() > 0) {
+            String number = enterEditText.getText().toString();
+            number = number.subSequence(0, number.length() - 1).toString();
+            enterEditText.setText(number);
+        }
+    }
+
+    public void onDeleteAll(View view) {
+        enterEditText.setText("");
+        signTextView.setText("");
+        resultTextView.setText("");
+        lastSign = "";
+        lastValue = null;
+    }
+
+    public void onChangeSign(View view) {
+        if (lastSign.equals("-")) {
+            lastSign = "+";
+            signTextView.setText(lastSign);
+        } else if (lastSign.equals("+")) {
+            lastSign = "-";
+            signTextView.setText(lastSign);
+        }
+    }
+
+    public void onSingClick(View view) {
+        String sing = ((Button) view).getText().toString();
+        String number = enterEditText.getText().toString();
+        if (number.length() > 0) {
+            try {
+                execute(Double.valueOf(number.replace(',', '.')), sing);
+            } catch (NumberFormatException ex) {
+                enterEditText.setText("");
+            }
+        }
+        lastSign = sing;
+        signTextView.setText(sing);
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void execute(Double number, String sing) {
+
+        if (lastValue == null) {
+            lastValue = number;
         } else {
-            textView.setText(textView.getText().toString().concat("1"));
+            if (lastSign.equals("=")) {
+                lastSign = sing;
+            }
+            checkSing(number);
         }
+
+        resultTextView.setText(format.format(lastValue));
+        enterEditText.setText("");
     }
 
-    public void onClickBtn2(View v) {
-        if (textView.getText() == "0") {
-            textView.setText("2");
-        } else {
-            textView.setText(textView.getText().toString().concat("2"));
-        }
-    }
-
-    public void onClickBtn3(View v) {
-        if (textView.getText() == "0") {
-            textView.setText("3");
-        } else {
-            textView.setText(textView.getText().toString().concat("3"));
-        }
-    }
-
-    public void onClickBtn4(View v) {
-        if (textView.getText() == "0") {
-            textView.setText("4");
-        } else {
-            textView.setText(textView.getText().toString().concat("4"));
-        }
-    }
-
-    public void onClickBtn5(View v) {
-        if (textView.getText() == "0") {
-            textView.setText("5");
-        } else {
-            textView.setText(textView.getText().toString().concat("5"));
-        }
-    }
-
-    public void onClickBtn6(View v) {
-        if (textView.getText() == "0") {
-            textView.setText("6");
-        } else {
-            textView.setText(textView.getText().toString().concat("6"));
-        }
-    }
-
-    public void onClickBtn7(View v) {
-        if (textView.getText() == "0") {
-            textView.setText("7");
-        } else {
-            textView.setText(textView.getText().toString().concat("7"));
-        }
-    }
-
-    public void onClickBtn8(View v) {
-        if (textView.getText() == "0") {
-            textView.setText("8");
-        } else {
-            textView.setText(textView.getText().toString().concat("8"));
-        }
-    }
-
-    public void onClickBtn9(View v) {
-        if (textView.getText() == "0") {
-            textView.setText("9");
-        } else {
-            textView.setText(textView.getText().toString().concat("9"));
-        }
-    }
-
-    public void onClickBtn0(View v) {
-        if (textView.getText() != "0") {
-            textView.setText(textView.getText().toString().concat("0"));
-        }
-    }
-
-    public void onClickBtnEqual(View v) {
-        second = Double.parseDouble(textView.getText().toString());
-        switch (oper) {
-            case "×":
-                result = first * second;
-                break;
-            case "+":
-                result = first + second;
+    private void checkSing(double number) {
+        switch (lastSign) {
+            case "=":
+                lastValue = number;
                 break;
             case "/":
-                result = first / second;
+                if (number == 0) {
+                    lastValue = 0.0;
+                } else {
+                    lastValue /= number;
+                }
+                break;
+            case "*":
+                lastValue *= number;
+                break;
+            case "+":
+                lastValue += number;
                 break;
             case "-":
-                result = first - second;
+                lastValue -= number;
                 break;
         }
-        if ((result % 1) == 0) {
-            textView.setText(String.format(Locale.US, "%.0f", result));
-            hasPoint = false;
-        } else {
-            textView.setText(format.format(result));
-            hasPoint = true;
-        }
-
-        textView2.setText("");
-        oper = "";
-    }
-
-    public void onClickBtnAC(View v) {
-        textView.setText("0");
-        textView2.setText("");
-        oper = "";
-        hasPoint = false;
-    }
-
-    public void onClickBtnMult(View v) {
-        if (oper.equals("×")) {
-            first = Double.parseDouble(textView2.getText().toString().replace("×", ""));
-            second = Double.parseDouble(textView.getText().toString());
-            result = first * second;
-            textView2.setText(String.format(Locale.US, "%f", result).concat("x"));
-        }
-        if (oper.equals("")) {
-            first = Double.parseDouble(textView.getText().toString());
-            textView2.setText(textView.getText().toString().concat("x"));
-        }
-        textView.setText("0");
-        oper = "×";
-        hasPoint = false;
-    }
-
-    public void onClickBtnDivide(View v) {
-        if (oper.equals("/")) {
-            first = Double.parseDouble(textView2.getText().toString().replace("/", ""));
-            second = Double.parseDouble(textView.getText().toString());
-            result = first / second;
-            textView2.setText(String.format(Locale.US, "%f", result).concat("/"));
-        }
-        if (oper.equals("")) {
-            first = Double.parseDouble(textView.getText().toString());
-            textView2.setText(textView.getText().toString().concat("/"));
-        }
-        textView.setText("0");
-        oper = "/";
-        hasPoint = false;
-    }
-
-    public void onClickBtnReverse(View v) {
-        if (textView.getText().toString().contains("-")) {
-            textView.setText(textView.getText().toString().replace("-", ""));
-        } else {
-            textView.setText("-".concat(textView.getText().toString()));
-        }
-    }
-
-    public void onClickBtnComma(View v) {
-        if (!hasPoint) {
-            textView.setText(textView.getText().toString().concat("."));
-            hasPoint = true;
-        }else{
-            textView.setText(textView.getText().toString().concat(""));
-        }
-    }
-
-    public void onClickBtnPercentage(View v) {
-        if (!textView2.getText().toString().isEmpty()) {
-            first = Double.parseDouble(textView2.getText().toString().replace(oper, ""));
-            second = Double.parseDouble(textView.getText().toString());
-            result = (first / 100) * second;
-            textView2.setText(String.format(Locale.US, "%f", result));
-        }
-    }
-
-    public void onClickBtnPlus(View v) {
-        if (oper.equals("+")) {
-            first = Double.parseDouble(textView2.getText().toString().replace("+", ""));
-            second = Double.parseDouble(textView.getText().toString());
-            result = first + second;
-            textView2.setText(String.format(Locale.US, "%f", result).concat("+"));
-        }
-        if (oper.equals("")) {
-            first = Double.parseDouble(textView.getText().toString());
-            textView2.setText(textView.getText().toString().concat("+"));
-        }
-        textView.setText("0");
-        oper = "+";
-        hasPoint = false;
-    }
-
-    public void onClickBtnMinus(View v) {
-        if (oper.equals("-")) {
-            first = Double.parseDouble(textView2.getText().toString().replace("-", ""));
-            second = Double.parseDouble(textView.getText().toString());
-            result = first - second;
-            textView2.setText(String.format(Locale.US, "%f", result).concat("-"));
-        }
-        if (oper.equals("")) {
-            first = Double.parseDouble(textView.getText().toString());
-            textView2.setText(textView.getText().toString().concat("-"));
-        }
-        textView.setText("0");
-        oper = "-";
-        hasPoint = false;
     }
 }
